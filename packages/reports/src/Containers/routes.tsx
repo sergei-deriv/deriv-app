c88e72ba61665bb3102bef3aa825f800fd1675ba
Sergei Baranovski <sergei@deriv.com>
2023-01-17 10:16:45 +0300

    chore: change RootStore type

--- packages/reports/src/Containers/routes.tsx
+++ packages/reports/src/Containers/routes.tsx
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router';
 import BinaryRoutes from 'Components/Routes';
 import { connect } from 'Stores/connect';
 import ErrorComponent from 'Components/Errors';
-import RootStore from 'Stores/index';
+import { TRootStore } from 'Stores/index';
 import { TRoutes } from '../Types';
 
 const Routes = ({ error, has_error, is_logged_in, is_logging_in, is_virtual, passthrough }: TRoutes) => {
@@ -17,7 +17,7 @@ const Routes = ({ error, has_error, is_logged_in, is_logging_in, is_virtual, pas
 // need to wrap withRouter around connect
 // to prevent updates on <BinaryRoutes /> from being blocked
 export default withRouter(
-    connect(({ client, common }: RootStore) => ({
+    connect(({ client, common }: TRootStore) => ({
         is_logged_in: client.is_logged_in,
         is_logging_in: client.is_logging_in,
         error: common.error,
